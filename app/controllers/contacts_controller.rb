4154f4bc41ce32d073d6e1502fb2dc62a080f9bc
Natalie Perez <NataliePerezEO@gmail.com>
2017-08-28 03:48:51 +0000

    Added create action to save to db

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,22 @@
 class ContactsController < ApplicationController
+   #Every time someone pulls up the form, rails will create a new, blank object
+   #and store it in front of instance(@) variable
    def new
+      @contact = Contact.new
    end
+   
+   def create
+      @contact = Contact.new(contact_params)
+      if @contact.save
+         redirect_to new_contact_path, notice: "Message sent."
+      else
+         redirect_to new_contact_path, notice: "Error occured"
+      end
+   end
+   
+   #whitelists
+   private
+      def contact_params
+         params.require(:contact).permit(:name, :email, :comments)
+      end
 end
