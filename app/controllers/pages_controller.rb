f630a6bdc0384363daa396267ec1b104238ed521
Natalie Perez <NataliePerezEO@gmail.com>
2017-08-28 21:23:30 +0000

    Added plan variables and params to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
+  # GET request for / home page
   def home
+    # .find will find the column where the ID equals the number inputted
+    # Plan.find(1).id will output 1   Plan.find(1).name will output basic
+    @basic_plan = Plan.find(1)
+    @pro_plan = Plan.find(2)
   end
   
   def about
