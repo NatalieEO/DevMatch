6ce5b15cca9d71b088545ec512c67bb2b2503333
Natalie Perez <NataliePerezEO@gmail.com>
2017-08-28 05:24:25 +0000

    Comments for routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   resources :contacts
-  #makes custom URL. contact/new changes to contact-us
+  # makes custom URL. contact/new changes to contact-us
+  # Whenver rails gets a get request for /contact-us, 
+  # rails knows to run the contacts controller new action.
   get 'contact-us', to: 'contacts#new'
 end
