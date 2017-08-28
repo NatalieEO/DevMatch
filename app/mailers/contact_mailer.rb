5b7eb46ed19f7bf17d8d6ff5ce24fe9cad68ca71
Natalie Perez <NataliePerezEO@gmail.com>
2017-08-28 06:01:43 +0000

    Added contact form mailer view

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,7 @@
 class ContactMailer < ActionMailer::Base
    default to: 'NataliePerezEO@gmail.com'
    
+   #instance variables defined here can be used in corresponding view file contact_email.html
    def contact_email(name, email, body)
       @name = name
       @email = email
