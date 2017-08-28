a9ea20591f962fc14b677222eaa8ed52739f322e
Natalie Perez <NataliePerezEO@gmail.com>
2017-08-28 05:41:42 +0000

    Added contact form mailer file

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,7 @@ Rails.application.routes.draw do
   # resources will have rails create 7 different routes, but we only need
   # two. new and create. GET and POST in HTTP verb
   resources :contacts, only: :create
-  # makes custom URL. contact/new changes to contact-us
-  # Whenver rails gets a get request for /contact-us, 
-  # rails knows to run the contacts controller new action.
+  # makes custom URL. contact/new changes to contact-us. Whenver rails gets a get request for /contact-us,
+  # rails knows to run the contacts controller new action. This gives us new/GET
   get 'contact-us', to: 'contacts#new', as: 'new_contact'
 end
