7f20d81d5bd1cbdf2638d641c1472d2104830696
Natalie Perez <NataliePerezEO@gmail.com>
2017-08-28 05:36:09 +0000

    Cleaned up contact form routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
-  resources :contacts
+  
+  # resources will have rails create 7 different routes, but we only need
+  # two. new and create. GET and POST in HTTP verb
+  resources :contacts, only: :create
   # makes custom URL. contact/new changes to contact-us
   # Whenver rails gets a get request for /contact-us, 
   # rails knows to run the contacts controller new action.
-  get 'contact-us', to: 'contacts#new'
+  get 'contact-us', to: 'contacts#new', as: 'new_contact'
 end
