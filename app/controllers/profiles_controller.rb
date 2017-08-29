3e2ac0748997fae6bbf8c10baa16f78dc9741196
Natalie Perez <NataliePerezEO@gmail.com>
2017-08-29 06:38:55 +0000

    Added comments for profile creation

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -8,10 +8,11 @@ class ProfilesController < ApplicationController
    
    # POST to /users/:user_id/profile
    def create
-      # Grab user_id from URL. Ensure that we have the user that is filling out the form
+      # Grab user_id from params Ensure that we have the user that is filling out the form
       @user = User.find( params[:user_id] )
       # Build a profile in the machines memory using the whitelisted profile fields
-      # Create profile linked to this specific user
+      # Create profile linked to this specific user. Build function provided by rails
+      # with this kind of association. Kind of like .new, but prefills foreign key.       images.build plural/has many relationship
       @profile = @user.build_profile( profile_params )
       
       if @profile.save
