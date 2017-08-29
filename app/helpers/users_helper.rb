e9a03a7ebb483d2c5f2fe7174e6da5a7f802de89
Natalie Perez <NataliePerezEO@gmail.com>
2017-08-29 20:24:04 +0000

    Added helper for job title icon

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,13 @@
+module UsersHelper
+   def job_title_icon
+      # Check for the user's job title and inject html safely into show.html.erb
+      # that will appropriate font awesome icon
+      if @user.profile.job_title == "Developer"
+         "<i class='fa fa-code'></i>".html_safe
+      elsif @user.profile.job_title == "Entrepreneur"
+         "<i class='fa fa-light-bulb-a'></i>".html_safe
+      elsif @user.profile.job_title == "Investor"
+         "<i class='fa fa-dollar'></i>".html_safe
+      end   
+   end   
+end
