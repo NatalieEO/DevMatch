e5f49454f41b499b456f574733000d2f59d574ea
Natalie Perez <NataliePerezEO@gmail.com>
2017-08-29 02:58:57 +0000

    Fixed alert notices given on contact-us page

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -18,5 +18,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
             end   
          end
       end
-   end   
+   end
+   
+   # Users can't try to manipulate sign up URL
+   private
+      def select_plan
+         unless (params[:plan] == '1' || params[:plan] == '2')
+            flash[:notice] = "Please select a membership plan to sign up."
+            redirect_to root_url
+         end
+      end
 end
