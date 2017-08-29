cdfa34a768b10f658e369ea458f1f70a70eb202e
Natalie Perez <NataliePerezEO@gmail.com>
2017-08-29 05:58:27 +0000

    Added user profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,6 +2,7 @@ class ProfilesController < ApplicationController
    
    # GET to /users/:user_id/profile/new
    def new
-      # Render blank profile details form
+      # Render blank profile details form. @profile needs to be instantiated to use in _form.html.erb
+      @profile = Profile.new
    end   
 end
