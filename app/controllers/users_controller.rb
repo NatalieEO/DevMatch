89e0f302ff049cfcfe87bddd5b01e8208d2c14de
Natalie Perez <NataliePerezEO@gmail.com>
2017-08-30 00:43:52 +0000

    Added users index view and action details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,17 @@ class UsersController < ApplicationController
    # not view any profiles or do any profile related actions
    before_action :authenticate_user!
    
+   def index
+      # Combines user and profile table into one query for faster load times
+      @users = User.includes(:profile)
+   end 
+   
    # GET to /users/:id     Show action when you want to show an individual resource
    def show
       # Find in the database the user with the id number that matches the URL
       @user = User.find( params[:id] )
    end
    
-   def index
-      
-   end   
+     
    
 end    
